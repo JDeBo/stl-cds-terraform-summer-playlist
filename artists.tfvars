0044c08d23aec99577f618eed9e4c3c4f77ea19d
Justin <justin.debo@gmail.com>
2021-07-18 21:21:32 -0500

    added mapping functionality

---
+++ artists.tfvars
@@ -0,0 +1,18 @@
+artists = {
+    "Mayday Parade" = {
+        name= "Mayday Parade"
+        limit= 20
+    },
+    "Quietdrive" = {
+        name= "Quietdrive"
+        limit= 20
+    }, 
+    "Yellowcard" = {
+        name= "Yellowcard"
+        limit= 10
+    },
+    "Fall Out Boy" = {
+        name= "Fall Out Boy"
+        limit= 30
+    }
+}
--- main.tf
+++ main.tf
@@ -12,24 +12,32 @@ variable "spotify_api_key" {
 }
 
 variable "artists" {
-  type = list
-  default = ["Mayday Parade", "Quietdrive", "Yellowcard", "Fall Out Boy"]
-  
+  type = map(object({
+  name = string
+  limit = number
+  }))
 }
+
+# variable "artists" {
+#   type = list
+#   default = ["Mayday Parade", "Quietdrive", "Yellowcard", "Fall Out Boy"]
+  
+# }
 provider "spotify" {
   api_key = var.spotify_api_key
 }
 
 resource "spotify_playlist" "playlist" {
-  name        = "Terraform Summer Playlist"
-  description = "This playlist was created by Terraform"
+  name        = "STL CDS Summer Playlist"
+  description = "This playlist was created by the fine CDS folks at Slalom STL"
   public      = true
 
-  tracks = concat(
-    [ for artist in var.artists: [for x in range(20): data.spotify_search_track.search_tracks["${artist}"].tracks[x].id]]
+  tracks = concat(flatten(
+    # [for x in range(20): data.spotify_search_track.search_tracks["Mayday Parade"].tracks[x].id]
+    [for artist in var.artists: [for x in range(artist.limit): data.spotify_search_track.search_tracks[artist.name].tracks[x].id]]
     # tolist([for x in range(20): data.spotify_search_track.Quietdrive.tracks[x].id]),
     # tolist([for x in range(10): data.spotify_search_track.Elliot-Yamin.tracks[x].id])
-  )
+  ))
   # tracks = [
   #   data.spotify_search_track.by_artist.tracks[0].id,
   #   data.spotify_search_track.by_artist.tracks[1].id,
@@ -38,20 +46,20 @@ resource "spotify_playlist" "playlist" {
 }
 
 data "spotify_search_track" "search_tracks" {
-  for_each = toset( var.artists )
-  artists = ["${each.key}"]
-  limit = 20
+  for_each = var.artists
+  artists = [each.value.name]
+  limit = each.value.limit
 }
 
-data "spotify_search_track" "Quietdrive" {
-  artists = ["Quietdrive"]
-  limit = 20
-}
+# data "spotify_search_track" "Quietdrive" {
+#   artists = ["Quietdrive"]
+#   limit = 20
+# }
 
-data "spotify_search_track" "Elliot-Yamin" {
-  artists = ["Elliot Yamin"]
-  limit = 10
-}
+# data "spotify_search_track" "Elliot-Yamin" {
+#   artists = ["Elliot Yamin"]
+#   limit = 10
+# }
 
 # output "tracks" {
 #   # value = data.spotify_search_track.by_artist.tracks
